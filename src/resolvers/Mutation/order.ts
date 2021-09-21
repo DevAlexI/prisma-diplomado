edaca94cc0e62a22756ea8a7a4032948b02ed5eb
devlex <48767282+DevAlexI@users.noreply.github.com>
2021-09-20 21:55:21 -0500

    Models & Resolvers

---
+++ src/resolvers/Mutation/order.ts
@@ -0,0 +1,12 @@
+import { Context } from "../../utils";
+
+export default {
+   /* async setPwd(parent, args, ctx: Context) {
+        const { pwd, token } = args.data;
+        const password = await bcrypt.hash(pwd, 10);
+        return await ctx.prisma.updateUser({ data: { password }, where: {token}});
+      },*/
+      //ctx.prisma.createOrder({data: {price}})
+    //createOrder: (parent, args, ctx: Context) => ctx.prisma.createOrder(args.data) 
+}
+ //createOrder: (parent, args, ctx: Context) => ctx.prisma.createOrder(args.data) 
